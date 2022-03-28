41209462dafbc8f9925b1b00d1d5023a0dd3ccdb
Joseph <2581690b@student.gla.ac.uk>
2022-03-28 15:52:43 +0100

    Commit

--- rango/forms.py
+++ rango/forms.py
@@ -1,33 +1,18 @@
+from xml.dom import UserDataHandler
 from django import forms
-from rango.models import Page, Category
+from rango.models import UserProfile
 from django.contrib.auth.models import User
 
-class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128,
-                           help_text="Please enter the category name.")
-    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
     
-    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
     class Meta:
-        model = Category
-        fields = ('name',)
+        model = User
+        fields = ('username', 'email', 'password')
         
-class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128,
-                            help_text="Please enter the title of the page.")
-    url = forms.URLField(max_length=200, 
-                         help_text="Please enter the URL of the page.")
-    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+class UserProfileForm(forms.ModelForm):
     class Meta:
-        model = Page
-        exclude = ('category',)
-        
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        url = cleaned_data.get('url')
-        
-        if url and not url.startswith('http://'):
-            url = f'http://{url}'
-            cleaned_data['url'] = url
-        return cleaned_data
+        model = UserProfile
+        fields = ('website', 'picture')
+
+
